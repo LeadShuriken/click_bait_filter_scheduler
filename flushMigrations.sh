02e363ca9dd945e6c1c9aac36383209126da45aa
LeadShuriken <d.s.mishev@gmail.com>
2021-05-08 18:38:08 +0200

    Initial 2

--- flushMigrations.sh
+++ flushMigrations.sh
@@ -2,18 +2,18 @@
 echo "Starting ..."
 
 echo ">> Deleting old migrations"
-find . -path "*/posrgresDb/migrations/*.py" -not -name "__init__.py" -delete
-find . -path "*/posrgresDb/migrations/*.pyc"  -delete
+find . -path "*/scheduler/migrations/*.py" -not -name "__init__.py" -delete
+find . -path "*/scheduler/migrations/*.pyc"  -delete
 
 # Optional
 echo ">> Deleting sqlite  (if exists) database"
 find . -name "db.sqlite3" -delete
 
 echo ">> Running manage.py makemigrations"
-python manage.py makemigrations
+python plugin_pg_scheduler/manage.py makemigrations
 
 echo ">> Running manage.py migrate"
-python manage.py migrate --database=primary
-python manage.py migrate --database=auth_db
+python plugin_pg_scheduler/manage.py migrate --database=primary
+python plugin_pg_scheduler/manage.py migrate --database=auth_db
 
 echo ">> Done"
