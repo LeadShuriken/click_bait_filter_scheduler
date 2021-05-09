44db3dba5adea64fe05843130ce8fea1f5975c0d
LeadShuriken <d.s.mishev@gmail.com>
2021-05-09 23:50:41 +0200

    Scheduler Authentication

--- plugin_pg_scheduler/builder/apps.py
+++ plugin_pg_scheduler/builder/apps.py
@@ -1,6 +1,5 @@
 from django.apps import AppConfig
 
-
-class SchedulerConfig(AppConfig):
+class BuilderConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'scheduler'
+    name = 'builder'
